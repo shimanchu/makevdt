20f852fd0e766b31fc9c4df1a95fcc2e48ba9df8
shimanchu <shimano@knd.biglobe.ne.jp>
2022-05-15 01:05:36 +0900

    First implementation

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true
+    },
+    extends: [
+        'standard'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest'
+    },
+    rules: {
+        indent: [
+            'error',
+            4
+        ],
+        semi: [
+            'error',
+            'always'
+        ]
+    }
+};
---
+++ makevdt.js
@@ -0,0 +1,144 @@
+const fs = require('fs');
+const program = require('commander');
+const PNGReader = require('png.js');
+const Iconv = require('iconv').Iconv;
+
+program
+    .description('Make vdt file')
+    .requiredOption('-p, --prefix <prefix>', 'input file prefix')
+    .requiredOption('-n, --file-num <fileNum>', 'number of input files')
+    .requiredOption('-t, --time-scale <timeScale>', 'time scale (60/30/20/15/12/10/6/5/4/3/2)')
+    .requiredOption('-a, --adpcm <adpcm>', 'ADPCM filename')
+    .requiredOption('-o, --outfile <outfile>', 'output filename')
+    .option('-c, --comment <comment>', 'comment', '')
+    .option('--digits <digits>', 'input file digits', 4)
+    .option('--adpcm-rate <adpcmRate>', 'ADPCM rate (0(no audio)/3(7.8KHz)/4(15.6KHz))', 4)
+    .option('--no-resize', 'do not resize image')
+    .showHelpAfterError(true)
+    .parse(process.argv);
+
+const options = program.opts();
+const iconv = new Iconv('UTF-8', 'SHIFT_JIS');
+const textEncoder = new TextEncoder();
+
+// Fixed parameters
+const flameLength = 30720;
+const flameWidth = 128;
+const flameHeight = 120;
+const isLittleEndian = false;
+
+let adpcmRateHz = 0;
+switch (options.adpcmRate) {
+case 3:
+    adpcmRateHz = 7800;
+    break;
+case 4:
+    adpcmRateHz = 15600;
+    break;
+}
+const convertedCommentBuffer = iconv.convert(options.comment);
+const headerSize = 0x7aaa + convertedCommentBuffer.length;
+const voiceSize = adpcmRateHz / 2 / (60 / options.timeScale);
+const bufferSize = headerSize + options.fileNum * (flameLength + voiceSize);
+const dataView = new DataView(new ArrayBuffer(bufferSize));
+
+let offset = 0;
+// Output header
+// - SiV
+dataView.setUint8(offset, textEncoder.encode('S')); offset++;
+dataView.setUint8(offset, textEncoder.encode('i')); offset++;
+dataView.setUint8(offset, textEncoder.encode('V')); offset++;
+// - comment
+convertedCommentBuffer.forEach(c => {
+    dataView.setUint8(offset, c); offset++;
+});
+dataView.setInt8(offset, 0x0a, isLittleEndian); offset++;
+// - poster image size
+dataView.setInt32(offset, flameLength, isLittleEndian); offset += 4;
+// - poster image data
+for (let y = 0; y < flameHeight; y++) {
+    for (let x = 0; x < flameWidth; x++) {
+        dataView.setInt16(offset, 0x1234, isLittleEndian); offset += 2;
+    }
+}
+// - quality
+dataView.setInt32(offset, 1, isLittleEndian); offset += 4;
+// - type
+dataView.setInt32(offset, 0, isLittleEndian); offset += 4;
+// - poster voice size
+dataView.setInt32(offset, voiceSize, isLittleEndian); offset += 4;
+// - poster voice data
+for (let i = 0; i < voiceSize; i++) {
+    dataView.setInt8(offset, 0x99, isLittleEndian); offset++;
+}
+// - time scale
+dataView.setInt32(offset, options.timeScale, isLittleEndian); offset += 4;
+// - ADPCM rate
+dataView.setInt32(offset, options.adpcmRate, isLittleEndian); offset += 4;
+// - flame number
+dataView.setInt32(offset, options.fileNum, isLittleEndian); offset += 4;
+
+// promise functions
+const readFilePromise = (filename) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, (error, buffer) => {
+            if (error) reject(error);
+            resolve(buffer);
+        });
+    });
+};
+
+const parsePromise = (reader) => {
+    return new Promise((resolve, reject) => {
+        reader.parse((error, png) => {
+            if (error) reject(error);
+            resolve(png);
+        });
+    });
+};
+
+// Output image and voice data
+(async () => {
+    const adpcmSize = fs.statSync(options.adpcm).size;
+    const adpcmBuf = fs.readFileSync(options.adpcm);
+    let adpcmOffset = 0;
+    for (let i = 1; i <= options.fileNum; i++) {
+        const pngFilename = options.prefix + ('0'.repeat(options.digits) + i).slice(-options.digits) + '.png';
+        const png = await parsePromise(new PNGReader(await readFilePromise(pngFilename)));
+        // - flame image data
+        for (let y = 0; y < flameHeight; y++) {
+            for (let x = 0; x < flameWidth; x++) {
+                const pixel = options.resize
+                    ? png.getPixel(
+                        Math.round(x / flameWidth * png.getWidth()),
+                        Math.round(y / flameHeight * png.getHeight()))
+                    : png.getPixel(
+                        Math.round(x),
+                        Math.round(y));
+                const converedPixel =
+                    (pixel[1] >> 3) << 11 | // G
+                    (pixel[0] >> 3) << 6 | // R
+                    (pixel[2] >> 3) << 1; // B
+                dataView.setInt16(offset, converedPixel, isLittleEndian); offset += 2;
+            }
+        }
+
+        // - flame voice data
+        for (let j = 0; j < voiceSize; j++) {
+            if (adpcmOffset < adpcmSize) {
+                dataView.setUint8(offset, adpcmBuf.readUint8(adpcmOffset)); adpcmOffset++; offset++;
+            }
+        }
+        if (i % (60 / options.timeScale) === 0 || i === Number(options.fileNum)) {
+            process.stdout.write(`\r${i} of ${options.fileNum} flames processed`);
+        }
+    }
+
+    // Output to file
+    fs.writeFile(options.outfile, dataView, (err) => {
+        if (err) throw err;
+        console.log(`\nOutput to ${options.outfile}`);
+    });
+})().catch((error) => {
+    console.log(error);
+});
